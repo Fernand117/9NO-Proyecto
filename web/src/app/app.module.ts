383c54336ff650ef380db9969fc3873723513c2b
Andrea SH <20183l101046@utcv.edu.mx>
2022-06-04 00:58:12 -0500

    ASH-CREATE-LOGIN
    Creación de inicio de sesión

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+;
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+ 
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
